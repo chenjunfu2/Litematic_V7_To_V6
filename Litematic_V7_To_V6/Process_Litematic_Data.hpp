7f27db740557155e49dc71047bc2af1409716446
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-02-16 03:56:18 +0800

    对齐

--- Litematic_V7_To_V6/Process_Litematic_Data.hpp
+++ Litematic_V7_To_V6/Process_Litematic_Data.hpp
@@ -28,52 +28,52 @@ void FixTileEntityId(NBT_Type::Compound &cpdTileEntity)
 
 	const std::vector<GuessNode> listIdGuess =
 	{
-		{MU8STR("Bees"), {}, MU8STR("minecraft:beehive")},
-		{MU8STR("bees"), {}, MU8STR("minecraft:beehive")},
+		{MU8STR("Bees"),					{},														MU8STR("minecraft:beehive")},
+		{MU8STR("bees"),					{},														MU8STR("minecraft:beehive")},
 
-		{MU8STR("TransferCooldown"), {MU8STR("Items")}, MU8STR("minecraft:hopper")},
-		{MU8STR("SkullOwner"), {}, MU8STR("minecraft:skull")},
+		{MU8STR("TransferCooldown"),		{MU8STR("Items")},										MU8STR("minecraft:hopper")},
+		{MU8STR("SkullOwner"),				{},														MU8STR("minecraft:skull")},
 
-		{MU8STR("Patterns"), {}, MU8STR("minecraft:banner")},
-		{MU8STR("patterns"), {}, MU8STR("minecraft:banner")},
+		{MU8STR("Patterns"),				{},														MU8STR("minecraft:banner")},
+		{MU8STR("patterns"),				{},														MU8STR("minecraft:banner")},
 
-		{MU8STR("Sherds"), {}, MU8STR("minecraft:decorated_pot")},
-		{MU8STR("sherds"), {}, MU8STR("minecraft:decorated_pot")},
+		{MU8STR("Sherds"),					{},														MU8STR("minecraft:decorated_pot")},
+		{MU8STR("sherds"),					{},														MU8STR("minecraft:decorated_pot")},
 
-		{MU8STR("last_interacted_slot"), {MU8STR("Items")}, MU8STR("minecraft:chiseled_bookshelf")},
-		{MU8STR("CookTime"), {MU8STR("Items")}, MU8STR("minecraft:furnace")},
-		{MU8STR("RecordItem"), {}, MU8STR("minecraft:jukebox")},
+		{MU8STR("last_interacted_slot"),	{MU8STR("Items")},										MU8STR("minecraft:chiseled_bookshelf")},
+		{MU8STR("CookTime"),				{MU8STR("Items")},										MU8STR("minecraft:furnace")},
+		{MU8STR("RecordItem"),				{},														MU8STR("minecraft:jukebox")},
 
-		{MU8STR("Book"), {}, MU8STR("minecraft:lectern")},
-		{MU8STR("book"), {}, MU8STR("minecraft:lectern")},
+		{MU8STR("Book"),					{},														MU8STR("minecraft:lectern")},
+		{MU8STR("book"),					{},														MU8STR("minecraft:lectern")},
 
-		{MU8STR("front_text"), {}, MU8STR("minecraft:sign")},
+		{MU8STR("front_text"),				{},														MU8STR("minecraft:sign")},
 
-		{MU8STR("BrewTime"), {}, MU8STR("minecraft:brewing_stand")},
-		{MU8STR("Fuel"), {}, MU8STR("minecraft:brewing_stand")},
+		{MU8STR("BrewTime"),				{},														MU8STR("minecraft:brewing_stand")},
+		{MU8STR("Fuel"),					{},														MU8STR("minecraft:brewing_stand")},
 
-		{MU8STR("LootTable"), {MU8STR("LootTableSeed")}, MU8STR("minecraft:suspicious_sand")},
-		{MU8STR("hit_direction"), {MU8STR("item")}, MU8STR("minecraft:suspicious_sand")},
+		{MU8STR("LootTable"),				{MU8STR("LootTableSeed")},								MU8STR("minecraft:suspicious_sand")},
+		{MU8STR("hit_direction"),			{MU8STR("item")},										MU8STR("minecraft:suspicious_sand")},
 
-		{MU8STR("SpawnData"), {}, MU8STR("minecraft:spawner")},
-		{MU8STR("SpawnPotentials"), {}, MU8STR("minecraft:spawner")},
+		{MU8STR("SpawnData"),				{},														MU8STR("minecraft:spawner")},
+		{MU8STR("SpawnPotentials"),			{},														MU8STR("minecraft:spawner")},
 
-		{MU8STR("normal_config"), {}, MU8STR("minecraft:trial_spawner")},
-		{MU8STR("shared_data"), {}, MU8STR("minecraft:vault")},
+		{MU8STR("normal_config"),			{},														MU8STR("minecraft:trial_spawner")},
+		{MU8STR("shared_data"),				{},														MU8STR("minecraft:vault")},
 
-		{MU8STR("pool"), {MU8STR("final_state"),MU8STR("placement_priority")}, MU8STR("minecraft:jigsaw")},
-		{MU8STR("author"), {MU8STR("metadata"),MU8STR("showboundingbox")}, MU8STR("minecraft:structure_block")},
-		{MU8STR("ExactTeleport"), {MU8STR("Age")}, MU8STR("minecraft:end_gateway")},
-		{MU8STR("Items"), {}, MU8STR("minecraft:chest")},
+		{MU8STR("pool"),					{MU8STR("final_state"), MU8STR("placement_priority")},	MU8STR("minecraft:jigsaw")},
+		{MU8STR("author"),					{MU8STR("metadata"), MU8STR("showboundingbox")},		MU8STR("minecraft:structure_block")},
+		{MU8STR("ExactTeleport"),			{MU8STR("Age")},										MU8STR("minecraft:end_gateway")},
+		{MU8STR("Items"),					{},														MU8STR("minecraft:chest")},
 
-		{MU8STR("last_vibration_frequency"), {MU8STR("listener")}, MU8STR("minecraft:sculk_sensor")},
-		{MU8STR("warning_level"), {MU8STR("listener")}, MU8STR("minecraft:sculk_shrieker")},
+		{MU8STR("last_vibration_frequency"),{MU8STR("listener")},									MU8STR("minecraft:sculk_sensor")},
+		{MU8STR("warning_level"),			{MU8STR("listener")},									MU8STR("minecraft:sculk_shrieker")},
 
-		{MU8STR("OutputSignal"), {}, MU8STR("minecraft:comparator")},
+		{MU8STR("OutputSignal"),			{},														MU8STR("minecraft:comparator")},
 
-		{MU8STR("facing"), {}, MU8STR("minecraft:piston")},
-		{MU8STR("extending"), {}, MU8STR("minecraft:piston")},
-		{MU8STR("x"), {MU8STR("y"),MU8STR("z")}, MU8STR("minecraft:piston")},
+		{MU8STR("facing"),					{},														MU8STR("minecraft:piston")},
+		{MU8STR("extending"),				{},														MU8STR("minecraft:piston")},
+		{MU8STR("x"),						{MU8STR("y"), MU8STR("z")},								MU8STR("minecraft:piston")},
 	};
 
 	//遍历猜测表，挨个匹配
